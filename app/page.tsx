e61dec22dabc8136b74ee22cfca14ed3bc65524a
Ogheneochuko <femioriodofe@gmail.com>
2024-11-01 00:25:42 +0100

    next.js dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import { lusitana } from '@/app/ui/fonts';
+
 import Image from 'next/image';
 
 export default function Page() {
@@ -34,14 +34,14 @@ export default function Page() {
         height={760}
         className="hidden md:block"
         alt="Screenshots of the dashboard project showing desktop version"
-      />
+      ></Image>
       <Image
        src="/hero-mobile.png"
        width={560}
        height={620}
        className= "block md:hidden"
        alt="Screenshots of the dashboard project showing mobile version"
-      />
+      ></Image>
         </div>
       </div>
     </main>
